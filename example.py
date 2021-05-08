bd7f3527385ccf9fc5f4c2568d5e1f523ec58265
veryacademy <veryacademy@gmail.com>
2021-05-08 15:05:36 +0100

    nc

--- example.py
+++ example.py
@@ -1,12 +1,31 @@
+import time
 import random
-x = random.randint(1,20)
-for i in range(0,4):
-    y = int(input("Guess the number between (1-20): "))
-    if i == 2:
-        print("This is your last guess!...")
-    if x == y:
-        print("You guessed correctly! ")
-        print(f"The right number was {x}")
-        break
-if y != x:
-    print(f"====\nSorry! The number was {x}\n====")
+name = input("What is your name? ")
+print ("Hello, " + name, "Time to play hangman!")
+time.sleep(1)
+print ("Start guessing...\n")
+time.sleep(0.5)
+## A List Of Secret Words
+words = ['python','programming','treasure','creative','medium','horror']
+word = random.choice(words)
+guesses = ''
+turns = 5
+while turns > 0:         
+    failed = 0             
+    for char in word:      
+        if char in guesses:    
+            print (char,end="")    
+        else:
+            print ("_",end=""),     
+            failed += 1    
+    if failed == 0:        
+        print ("\nYou won") 
+        break              
+    guess = input("\nguess a character:") 
+    guesses += guess                    
+    if guess not in word:  
+        turns -= 1        
+        print("\nWrong")    
+        print("\nYou have", + turns, 'more guesses') 
+        if turns == 0:           
+            print ("\nYou Lose") 
